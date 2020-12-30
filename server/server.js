6c0d5749e08a592351cde82ff68c0a52ec3232ca
Víctor Hugo <victorhugo.webdeveloper@gmail.com>
2020-12-29 21:17:52 -0600

    Commit de respaldo

---
+++ server/server.js
@@ -0,0 +1,55 @@
+require('./config/config')
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+//Parse application/x-www-form.urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+//Parse application/json
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+  res.json('Hola Mundo')
+})
+
+app.get('/usuario', function (req, res) {
+  res.json('get Usuario')
+})
+
+app.post('/usuario', function (req, res) {
+
+  let body = req.body
+
+  if(body.nombre === undefined) {
+
+    res.status(400).json({
+      ok: false,
+      mensaje: 'El nombre es necesario',
+    })
+  
+  } else {
+    res.json({
+      persona: body
+    })
+  }
+
+})
+
+app.put('/usuario/:id', function (req, res) {
+  
+  let id = req.params.id
+  
+  res.json({
+    id
+  })
+})
+
+app.delete('/usuario', function (req, res) {
+  res.json('delete Usuario')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log('Escuchando el puerto: ', 3000)
+})
